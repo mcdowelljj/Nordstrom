93cc4be412b6fa44c6dd23ea8b6e8dfa7fb6492c
mcdowelljj <runnerjohn1213@yahoo.com>
2015-12-28 19:23:05 -0500

    First Commit

---
+++ pets.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('PetApp', [])
+.controller('PetCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get('https://data.kingcounty.gov/resource/murn-chih.json?record_type=FOUND').then(function(reponse){
+		console.log(response.data);
+		$scope.pets = response.data;
+	});
+}]);
+
